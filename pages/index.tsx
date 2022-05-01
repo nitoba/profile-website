76e29ef4af624d38db08b4effad5a662ce987ef8
Bruno Alves <bruno@sthorm.io>
2022-04-30 21:03:51 -0300

    add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
-import type { NextPage } from 'next'
-import styles from '../styles/home.module.css'
-import AboutMe from './components/about-me'
-import ContactMe from './components/contact-me'
-import Footer from './components/footer'
-import Header from './components/header'
-import TechsUsed from './components/techs-used'
-import Testimonies from './components/testimonies'
+import type { NextPage } from "next";
+import styles from "../styles/home.module.css";
+import AboutMe from "./components/about-me";
+import ContactMe from "./components/contact-me";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import TechsUsed from "./components/techs-used";
+import Testimonies from "./components/testimonies";
 const Home: NextPage = () => {
   return (
     <>
-    <div className={styles.container}>
-      <Header />
-      <AboutMe />
-      <TechsUsed />
-      <Testimonies />
-      <ContactMe />
-    </div>
-    <Footer />
+      <title>Bruno Alves - Full Stack Dev</title>
+      <div className={styles.container}>
+        <Header />
+        <AboutMe />
+        <TechsUsed />
+        <Testimonies />
+        <ContactMe />
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
