ab9f8294db35150526a838d2b6425800c33e133f
Bruno Alves <bruno@sthorm.io>
2023-02-24 13:28:50 -0300

    feat: update next version and add new api metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
+import type { Metadata } from 'next'
 import { ReactNode } from 'react'
 import { Poppins } from '@next/font/google'
 import { Header } from '../components/header'
 import { Footer } from '../components/footer'
+
+import '../styles/globals.css'
 
 type Props = {
   children: ReactNode
@@ -13,52 +15,47 @@ const poppins = Poppins({
   subsets: ['latin'],
 })
 
+export const metadata: Metadata = {
+  title: 'Bruno Alves - Full Stack Dev',
+  description:
+    'Seja bem vindo ao meu site pessoal. Aqui você pode conferir um pouco sobre minhas experiências como desenvolvedor e navegar sobre os projetos que desenvolvo',
+  openGraph: {
+    title: 'Bruno Alves - Full Stack Dev',
+    description:
+      'Seja bem vindo ao meu site pessoal. Aqui você pode conferir um pouco sobre minhas experiências como desenvolvedor e navegar sobre os projetos que desenvolvo',
+    url: 'https://nitodev.vercel.app',
+    siteName: 'Bruno Alves - Full Stack Dev',
+    images: [
+      {
+        url: 'https://i.ibb.co/m8Bfyq1/profile-2.jpg',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'pt-BR',
+    type: 'website',
+  },
+  twitter: {
+    title: 'Bruno Alves - Full Stack Dev',
+    site: 'https://nitodev.vercel.app',
+    card: 'summary_large_image',
+    description:
+      'Seja bem vindo ao meu site pessoal. Aqui você pode conferir um pouco sobre minhas experiências como desenvolvedor e navegar sobre os projetos que desenvolvo',
+    images: [
+      {
+        url: 'https://i.ibb.co/m8Bfyq1/profile-2.jpg',
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
+}
+
 export default function RootLayout({ children }: Props) {
   return (
     <html className={poppins.className}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content="Seja bem vindo ao meu site pessoal. Aqui você pode conferir um pouco sobre minhas experiências como desenvolvedor e navegar sobre os projetos que desenvolvo"
-        />
-
-        {/* Facebook Meta Tags */}
-        <meta
-          property="og:url"
-          content="https://profile-website-9hrnnnzv4-nitoba.vercel.app/"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Bruno Alves - Full Stack Dev" />
-        <meta
-          property="og:description"
-          content="Seja bem vindo ao meu site pessoal. Aqui você pode conferir um pouco sobre minhas experiências como desenvolvedor e navegar sobre os projetos que desenvolvo"
-        />
-        <meta
-          property="og:image"
-          content="https://i.ibb.co/m8Bfyq1/profile-2.jpg"
-        />
-
-        {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:domain"
-          content="profile-website-9hrnnnzv4-nitoba.vercel.app"
-        />
-        <meta
-          property="twitter:url"
-          content="https://profile-website-9hrnnnzv4-nitoba.vercel.app/"
-        />
-        <meta name="twitter:title" content="Bruno Alves - Full Stack Dev" />
-        <meta
-          name="twitter:description"
-          content="Seja bem vindo ao meu site pessoal. Aqui você pode conferir um pouco sobre minhas experiências como desenvolvedor e navegar sobre os projetos que desenvolvo "
-        />
-        <meta
-          name="twitter:image"
-          content="https://i.ibb.co/m8Bfyq1/profile-2.jpg"
-        />
-        <title>Bruno Alves - Full Stack Dev</title>
       </head>
       <body>
         <div className="w-full max-w-[1220px] mx-auto px-[1.5rem]">
